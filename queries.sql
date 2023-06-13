c220edd7d7dd0b03e75fc4cf66a8e7df0e85fbc2
naanmohammed <naanahmed24@gmail.com>
2023-06-14 00:48:06 +0300

    Update queries

--- queries.sql
+++ queries.sql
@@ -37,8 +37,8 @@ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
 SELECT COUNT(*) FROM animals;
-SELECT COUNT(*) FROM animals WHERE escape_attempts > 0;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
-SELECT neutered, COUNT(*) AS escape_attempts FROM animals GROUP BY neutered ORDER BY escape_attempts DESC;
+SELECT neutered, MAX(escape_attempts) AS max_escape_attempts FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) AS avg_escape_attempts FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31' GROUP BY species;
